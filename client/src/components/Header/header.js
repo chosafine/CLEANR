3b7102033fc33b2874bd9762ecaf5e42ac40ecf3
Clarisse Moraes <57908323+cmoraes2013@users.noreply.github.com>
2020-02-17 16:02:13 -0500

    Adding components (#2)
    * .env file added
    
    * added header component
    
    * Delete .env
    
    who raised you
    
    * Delete package-lock.json
    
    🙄
    
    Co-authored-by: Cristian Colocho <chosafine@icloud.com>

---
+++ client/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (    
+    <div className = "company-name"> 
+    <h1>CLEANR</h1>
+    <h2>Residential & Commercial Cleaning</h2> 
+    <p>Est. 2001 </p>
+    </div>    
+  );
+}
+
+export default Header
