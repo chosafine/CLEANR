18c6fe65f61d0a3a8f2e6960f12fce47a6e1240b
Clarisse <you@example.com>
2020-02-17 17:06:13 -0500

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
-import header from './components/Header/header'
+import Header from './components/Header/header';
 
 function App() {
   return (
-      <header />
+      <Header />
   );
 }
 
