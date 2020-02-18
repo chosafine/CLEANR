5ded6af07686933281e7ded5aee8447235e58eeb
Clarisse <you@example.com>
2020-02-18 13:34:52 -0500

    rendered components on page

---
+++ client/src/components/Authentication/authentication.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./authentication.css"
+
+function Authentication() {
+    return (
+        <div className="company-auth">
+            SIGN UP | LOGIN
+        </div>
+    );
+}
+
+export default Authentication
