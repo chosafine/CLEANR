6f9395284be0e669f2df9ef1de3bf47c29c41144
Cristian Colocho <chosafine@icloud.com>
2020-02-20 11:08:20 -0500

    working on store functionality

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -2,31 +2,23 @@
 /*
  * action types
  */
-export const ADD_ADDRESS = 'ADD_ADDRESS'
-export const SQUARE_FT = 'SQUARE_FT'
-export const FREQUENCY = 'FREQUENCY'
-export const NEXT = 'NEXT'
-export const NOTES = 'NOTES'
-export const RATE = 'RATE'
-export const NEW = 'NEW'
+export const QUESTION = "QUESTION";
+export const NEW = "NEW";
+export const SET_NEXT_QUESTION = "SET_NEXT_QUESTION";
 /*
  * action creators
  */
-export function addAddress(text) {
-  return { type: ADD_ADDRESS, text }
+export function addQuestion(value, price) {
+  return {
+    type: QUESTION,
+    value,
+    price
+  };
 }
-export function addSquareFt(text) {
-  return { type: SQUARE_FT, text }
+export function addNew(bool) {
+  return { type: NEW, bool };
 }
-export function addFrequency(text) {
-  return { type: FREQUENCY, text }
-}
-export function addNext(text) {
-  return { type: NEXT, text }
-}
-export function addNotes(text) {
-  return { type: NOTES, text }
-}
-export function addNew(text) {
-  return { type: NEW, text }
+
+export function setNextQuestion(filter) {
+  return { type: SET_NEXT_QUESTION, filter };
 }
---
+++ client/src/utils/index.js
@@ -0,0 +1,3 @@
+import { createStore } from 'redux'
+import cleaningApp from './reducers'
+const store = createStore(cleaningApp)
