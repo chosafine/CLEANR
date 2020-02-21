a1c065ac27d8f7f2a88e548909d947185439fca0
Clarisse <you@example.com>
2020-02-21 13:49:12 -0500

    added buttons

--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./card.css";
 
+
 function Card(props) {
 
   return (
     <div className="company-card">
       <p>{props.title}</p>
       {props.choices.map(choice => (
-      <div>
-        <input type="radio" key="{choice.value}" defaultValue="{choice.value}" />
-        <label htmlFor="{choice.value}">{choice.value}</label>
-      </div>
+        <button type="checkbox" key="{choice.value}">{choice.value}</button> 
       ))}
     </div>
   );
