000ef61ae59cbe40977f6e202991eb4adce7db1f
Cristian Colocho <chosafine@icloud.com>
2020-02-24 14:38:02 -0500

    updating post route

--- client/src/components/Submit/submit.js
+++ client/src/components/Submit/submit.js
@@ -6,10 +6,15 @@ import StartOver from "../Start Over/startover";
 class Submit extends React.Component {
   handleSubmit = () => {
   	const currentStore = store.getState();
+  	let postStore = {};
+  	
+  	for (const property in currentStore.items) {
+  		postStore[currentStore.items[property].name] = currentStore.items[property].text;
+	}
   
     axios.post(
   	'http://localhost:3001/api/cleaning',
-  	currentStore,
+  	postStore,
   	{ headers: { 'Content-Type': 'application/json' } }
 	).then(response => {
 		console.log(response.data)
