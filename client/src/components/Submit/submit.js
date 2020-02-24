4edadce30afcf5a63b44904e4401830f2c80c58b
Cristian Colocho <chosafine@icloud.com>
2020-02-24 14:59:20 -0500

    added eslint support to client project

--- client/src/components/Submit/submit.js
+++ client/src/components/Submit/submit.js
@@ -5,20 +5,21 @@ import StartOver from "../Start Over/startover";
 
 class Submit extends React.Component {
   handleSubmit = () => {
-  	const currentStore = store.getState();
-  	let postStore = {};
-  	
-  	for (const property in currentStore.items) {
-  		postStore[currentStore.items[property].name] = currentStore.items[property].text;
-	}
-  
-    axios.post(
-  	'http://localhost:3001/api/cleaning',
-  	postStore,
-  	{ headers: { 'Content-Type': 'application/json' } }
-	).then(response => {
-		console.log(response.data)
-	})
+    const currentStore = store.getState();
+    let postStore = {};
+
+    for (const property in currentStore.items) {
+      postStore[currentStore.items[property].name] =
+        currentStore.items[property].text;
+    }
+
+    axios
+      .post("http://localhost:3001/api/cleaning", postStore, {
+        headers: { "Content-Type": "application/json" }
+      })
+      .then(response => {
+        console.log(response.data);
+      });
   };
 
   render() {
