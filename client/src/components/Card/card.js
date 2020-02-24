bb6751fa10e11644305b96be2c62d1453b21f953
Clarisse Moraes <57908323+cmoraes2013@users.noreply.github.com>
2020-02-23 19:26:01 -0500

    Start over component (#16)
    * added functionality to buttons to reset store
    
    * start over button working
    
    Co-authored-by: Cristian Colocho <chosafine@icloud.com>

--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -1,53 +1,45 @@
 import React from "react";
 import "./card.css";
+import StartOver from "../Start Over/startover";
 import { connect } from "react-redux";
 import { addQuestion, reset } from "../../utils/actions";
+
 
 
 class AddQuestions extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  name: "",
-    				value: "" };
-    this.state = { disabled: false };
-    
+    this.state = { value: "" };
+    this.state = { disabled: false }
   }
 
-  handleAddQuestion = (value1, value2) => {
-    this.setState({ name: value1, value: value2 }, () => {
-      this.props.addQuestion(this.state.name, this.state.value);
-      this.setState({ disabled: !this.state.disabled });
-    });
+  handleAddQuestion = value => {
+    this.setState({ value }, () => {
+      this.props.addQuestion(this.state.value);
+      this.setState({ disabled: !this.state.disabled })
+    }); 
   };
+
+    render() {
+      return (
+        <div className="company-card">
+          <p>{this.props.title}</p>
+          {this.props.choices.map(choice => (
+            <button
+              key={`${choice.value}`}
+              value={`${choice.value}`}
+              id="myBtn"
+              disabled={(this.state.disabled) ? "disabled" : ""}
+              onClick={e => this.handleAddQuestion(e.target.value)}
   
-  handleStartOver = () => {
-      this.props.reset();
+            >
+              {choice.value}
+            </button>
+          ))}
+          <StartOver />
+        </div>
+      );
   }
-  render = props => {
-    const startOver = (
-      <button type="button" className="btn" onClick={this.handleStartOver()}> Start Over!
-      </button>
-        );
-
-    return (
-      <div className="company-card">
-        <p>{this.props.title}</p>
-        {this.props.choices.map(choice => (
-          <button
-            key={`${choice.value}`}
-            value={`${choice.value}`}
-            datavalue={this.props.name}
-            disabled={(this.state.disabled) ? "disabled" : ""}
-            onClick={e => this.handleAddQuestion(e.target.getAttribute("datavalue"), e.target.value)}
-            id="myBtn"
-          >
-            {choice.value}
-          </button>
-        ))}
-        {startOver}
-      </div>
-    );
-  };
-}
+};
 
 export default connect(null, { addQuestion, reset })(AddQuestions);
