16ab8571194be4e91b1572876ef022d064a5c9a9
Clarisse Moraes <57908323+cmoraes2013@users.noreply.github.com>
2020-02-25 15:57:27 -0500

    rendered price (#23)

---
+++ client/src/components/Estimate/estimate.js
@@ -0,0 +1,64 @@
+import React from "react";
+import store from "../../utils/index";
+import {
+    homeCleaning,
+    officeCleaning,
+    venueCleaning
+} from "../../utils/questions";
+
+class Estimate extends React.Component {
+    state = { price: [] }
+
+    componentDidMount() {
+        this.observeStore(store, this.onChange);
+    }
+
+    onChange = (store, object) => {
+        let totalPrice = 0;
+
+        for (const property in store.items) {
+            const name = store.items[property].name
+            const value = store.items[property].text
+
+            object.questions.forEach(element => {
+                if (element.name === name) {
+                    for (const choice in element.choices) {
+                        if (element.choices[choice].value == value) {
+                            let price = element.choices[choice].price
+                            totalPrice += price;
+                        }
+                    }
+                }
+            });
+        }
+        return this.setState({price: totalPrice});
+    }
+
+    observeStore = (store, onChange) => {
+        let currentState;
+        function handleChange() {
+            let nextState = store.getState();
+            if (nextState !== currentState) {
+                currentState = nextState;
+                onChange(currentState, homeCleaning);
+            }
+        }
+        let unsubscribe = store.subscribe(handleChange);
+        handleChange();
+        return unsubscribe;
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Current Price: {this.state.price}</p>
+            </div>
+        )
+    }
+
+}
+
+export default Estimate;
+
+
+
--- client/src/components/Submit/submit.js
+++ client/src/components/Submit/submit.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import store from "../../utils/index";
+import Estimate from "../Estimate/estimate";
 
 
 class Submit extends React.Component {
@@ -31,7 +32,7 @@ class Submit extends React.Component {
         <button type="button" className="btn" onClick={this.handleSubmit}>
           Submit
         </button>
-        
+        <Estimate />
       </div>
     );
   }
