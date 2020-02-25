79ea0c9f6df24ffefb6b6198dd61984f28ae22b3
Clarisse <you@example.com>
2020-02-25 17:41:07 -0500

    all components rendering

--- client/src/components/Questions/question.js
+++ client/src/components/Questions/question.js
@@ -3,20 +3,41 @@ import Card from '../Card/card';
 import Submit from '../Submit/submit';
 import StartOver from "../Start Over/startover";
 import Booking from "../Booking/booking";
+import Estimate from "../Estimate/estimate";
 
-function Question(props) {
-  const { questions } = props;
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      {questions.map((question, index) => (
-        <Card key={index} {...question} />
-      ))}
-      <Submit />
-      <StartOver />
-      <Booking />
-    </div>
-  );
+    this.state = {
+      showCards: true,
+      showBookingInfo: false
+    };
+  }
+
+  nextQuote = () => {
+    this.setState({
+      showCards: false,
+      showBookingInfo: true
+    })
+  }
+
+  render() {
+    const { questions } = this.props;
+
+    const questionList = questions.map((question, index) => (
+      <Card key={index} {...question} />
+    ));
+
+    const getEstimate = <button onClick={this.nextQuote}>Get Free Quote!</button>;
+
+    return (
+      <div>
+        {this.state.showCards ? <div>{questionList} <StartOver /> {getEstimate}</div> : null}
+        {this.state.showBookingInfo ? <div><Estimate /> <Booking /></div> : null}
+      </div>
+    );
+  }
 }
 
 export default Question;
